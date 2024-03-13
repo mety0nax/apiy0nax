def2c7e1f26cf329213ebe7d47914421630896f7
Mihails Fraimans <45516034+mety0nax@users.noreply.github.com>
2024-03-14 01:04:11 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import os
 import fastapi
-# import uvicorn
+import uvicorn
 
 # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
 #                                                             #
